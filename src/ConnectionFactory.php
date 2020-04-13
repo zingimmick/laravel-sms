261fcdd50208253080f2dad4a1a61a69d849d2c8
zingimmick <zingimmick@outlook.com>
2020-04-13 14:35:16 +0800

    美联

--- src/ConnectionFactory.php
+++ src/ConnectionFactory.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Zing\LaravelSms;
-
 
 use Zing\LaravelSms\Exceptions\InvalidArgumentException;
 
@@ -10,18 +8,21 @@ class ConnectionFactory
 {
     /**
      * @param array $config
+     *
      * @return \Zing\LaravelSms\Contracts\Driver
+     *
      * @throws \Zing\LaravelSms\Exceptions\InvalidArgumentException
      */
     public function make(array $config)
     {
-        if (!isset($config['driver'])) {
+        if (! isset($config['driver'])) {
             throw new InvalidArgumentException('A driver must be specified.');
         }
         $driver = $config['driver'];
         if (class_exists($driver)) {
             return new $driver($config);
         }
+
         throw new InvalidArgumentException("Unsupported driver [{$config['driver']}].");
     }
 }
