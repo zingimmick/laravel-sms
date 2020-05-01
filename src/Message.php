e05f066af795179f62744522c09c9da476eff1b4
zingimmick <zingimmick@outlook.com>
2020-05-02 03:41:09 +0800

    Marking class Message as deprecated.

--- src/Message.php
+++ src/Message.php
@@ -4,44 +4,9 @@ declare(strict_types=1);
 
 namespace Zing\LaravelSms;
 
-use Illuminate\Bus\Queueable;
-use Overtrue\EasySms\Contracts\MessageInterface;
-
 /**
  * Class Message.
  */
-class Message extends \Overtrue\EasySms\Message
+class Message extends SmsMessage
 {
-    use Queueable;
-
-    /**
-     * @param string|callable $content
-     *
-     * @return static
-     */
-    public static function text($content)
-    {
-        return (new static([], MessageInterface::TEXT_MESSAGE))->setContent($content);
-    }
-
-    /**
-     * @param string|callable $content
-     *
-     * @return static
-     */
-    public static function voice($content)
-    {
-        return (new static([], MessageInterface::VOICE_MESSAGE))->setContent($content);
-    }
-
-    /**
-     * @param string|callable $template
-     * @param array|callable $data
-     *
-     * @return \Zing\LaravelSms\Message
-     */
-    public static function fromTemplate($template, $data)
-    {
-        return static::text('')->setTemplate($template)->setData($data);
-    }
 }
