b87d4caf3a6180d5445eb3ee74ee92ffde47ded2
zingimmick <zingimmick@outlook.com>
2020-05-05 18:40:11 +0800

    Remove usages of deprecated method.

--- tests/Drivers/DriverTest.php
+++ tests/Drivers/DriverTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Zing\LaravelSms\Tests\Drivers;
 
 use Exception;
-use Mockery;
+use Illuminate\Support\Facades\Event;
 use Overtrue\EasySms\Message;
 use Overtrue\EasySms\PhoneNumber;
 use Zing\LaravelSms\Connectors\Connector;
@@ -18,21 +18,21 @@ class DriverTest extends TestCase
     {
         $number = new PhoneNumber(18188888888);
         $message = new Message([]);
-        $driver = Mockery::mock(Connector::class . '[sending]', [[]]);
-        $driver->shouldReceive('sending')->andThrow(new Exception('test'));
+        Event::fake();
+        Event::shouldReceive('dispatch')->andThrow(new Exception('test'));
         $this->expectException(CouldNotSendNotification::class);
         $this->expectExceptionMessage('test');
-        $driver->send($number, $message);
+        (new Connector([]))->send($number, $message);
     }
 
     public function testStaticException(): void
     {
         $number = new PhoneNumber(18188888888);
         $message = new Message([]);
-        $driver = Mockery::mock(Connector::class . '[sending]', [[]]);
-        $driver->shouldReceive('sending')->andThrow(new CouldNotSendNotification('test'));
+        Event::fake();
+        Event::shouldReceive('dispatch')->andThrow(new CouldNotSendNotification('test'));
         $this->expectException(CouldNotSendNotification::class);
         $this->expectExceptionMessage('test');
-        $driver->send($number, $message);
+        (new Connector([]))->send($number, $message);
     }
 }
