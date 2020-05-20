d2d27958512a781e4f343a1221a76071b263a301
zingimmick <zingimmick@outlook.com>
2020-05-20 12:42:32 +0800

    test with pest.

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -7,11 +7,14 @@ namespace Zing\LaravelSms\Tests;
 use Overtrue\EasySms\Contracts\MessageInterface;
 use Zing\LaravelSms\SmsMessage;
 
-test('create message with static call', function (): void {
-    $message = SmsMessage::text('');
-    assertSame(MessageInterface::TEXT_MESSAGE, $message->getMessageType());
-    $message = SmsMessage::voice('');
-    assertSame(MessageInterface::VOICE_MESSAGE, $message->getMessageType());
-    $message = SmsMessage::fromTemplate('', []);
-    assertSame(MessageInterface::TEXT_MESSAGE, $message->getMessageType());
-});
+test(
+    'create message with static call',
+    function (): void {
+        $message = SmsMessage::text('');
+        assertSame(MessageInterface::TEXT_MESSAGE, $message->getMessageType());
+        $message = SmsMessage::voice('');
+        assertSame(MessageInterface::VOICE_MESSAGE, $message->getMessageType());
+        $message = SmsMessage::fromTemplate('', []);
+        assertSame(MessageInterface::TEXT_MESSAGE, $message->getMessageType());
+    }
+);
--- tests/PhoneNumberTest.php
+++ tests/PhoneNumberTest.php
@@ -6,9 +6,12 @@ namespace Zing\LaravelSms\Tests;
 
 use Zing\LaravelSms\PhoneNumber;
 
-it('can notify', function (): void {
-    $phone = new PhoneNumber('18188888888');
-    $notification = new VerifyCode();
-    prepareLoggerExpectation()->with(sendString($phone->routeNotificationForSms($notification), $notification->toSms($phone)));
-    $phone->notify($notification);
-});
+it(
+    'can notify',
+    function (): void {
+        $phone = new PhoneNumber('18188888888');
+        $notification = new VerifyCode();
+        prepareLoggerExpectation()->with(sendString($phone->routeNotificationForSms($notification), $notification->toSms($phone)));
+        $phone->notify($notification);
+    }
+);
