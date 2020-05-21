ac41a84f12019abdb2f48582f01bea8e692d27da
zingimmick <zingimmick@outlook.com>
2020-05-21 10:41:33 +0800

    Fix style.

--- src/SmsNumber.php
+++ src/SmsNumber.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Zing\LaravelSms;
 
 use Illuminate\Notifications\Notifiable;
+use Overtrue\EasySms\PhoneNumber;
 
-class SmsNumber extends \Overtrue\EasySms\PhoneNumber
+class SmsNumber extends PhoneNumber
 {
     use Notifiable;
 
