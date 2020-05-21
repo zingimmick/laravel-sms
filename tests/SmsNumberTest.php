7c0c0ca2628c19e7be663ae8a6044e33f3ac4093
zingimmick <zingimmick@outlook.com>
2020-05-21 11:09:31 +0800

    Remove PhoneNumber.

--- tests/SmsNumberTest.php
+++ tests/SmsNumberTest.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Zing\LaravelSms\Tests;
 
-use Zing\LaravelSms\PhoneNumber;
+use Zing\LaravelSms\SmsNumber;
 
 it(
     'can notify',
     function (): void {
-        $phone = new PhoneNumber('18188888888');
+        $phone = new SmsNumber('18188888888');
         $notification = new VerifyCode();
         prepareLoggerExpectation()->with(sendString($phone->routeNotificationForSms($notification), $notification->toSms($phone)));
         $phone->notify($notification);
