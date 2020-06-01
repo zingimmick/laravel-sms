a9b676a8ca5a742fca3c425ee55d08370c7ef64a
Zing <zingimmick@outlook.com>
2020-06-01 19:02:51 +0800

    Support Lumen (#30)
    * Support Lumen
    
    * Support Lumen
    
    * Support Lumen

---
+++ tests/Lumen/ServiceProviderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zing\LaravelSms\Tests\Lumen;
+
+use Zing\LaravelSms\Tests\Concerns\ServiceProviderTests;
+
+class ServiceProviderTest extends LumenTestCase
+{
+    use ServiceProviderTests;
+}
