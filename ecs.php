30f50e90fd22899a922ea2299f0a2808ae006cb7
zingimmick <zingimmick@outlook.com>
2020-07-22 03:10:38 +0800

    Fix style

--- ecs.php
+++ ecs.php
@@ -6,20 +6,27 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symplify\EasyCodingStandard\Configuration\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__.'/vendor/zing/coding-standard/config/config.php');
+    $containerConfigurator->import(__DIR__ . '/vendor/zing/coding-standard/config/config.php');
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::SETS, [
-        'psr12',
-        'php70',
-        'php71',
-        'dead-code',
-        'clean-code',
-        'common',
-    ]);
-    $parameters->set(Option::PATHS, [
-        'config',
-        'src',
-        'tests',
-    ]);
-
+    $parameters->set(
+        Option::SETS,
+        [
+            'psr12',
+            'php70',
+            'php71',
+            'dead-code',
+            'clean-code',
+            'common',
+        ]
+    );
+    $parameters->set(
+        Option::PATHS,
+        [
+            'config',
+            'src',
+            'tests',
+            'ecs.php',
+            'rector.php',
+        ]
+    );
 };
--- rector.php
+++ rector.php
@@ -14,32 +14,43 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::SETS, [
-        SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION,
-        SetList::ARRAY_STR_FUNCTIONS_TO_STATIC_CALL,
-        SetList::CELEBRITY,
-        //        'doctrine',
-        SetList::PHPSTAN,
-        SetList::PHPUNIT_CODE_QUALITY,
-        SetList::SOLID,
-        SetList::DOCTRINE_CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::CODE_QUALITY,
-        SetList::PHP_70,
-        SetList::PHP_71,
-        SetList::PHP_72,
-    ]);
-    $parameters->set(Option::EXCLUDE_RECTORS, [
-        FinalizeClassesWithoutChildrenRector::class,
-        ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
-        RecastingRemovalRector::class,
-        RepeatedLiteralToClassConstantRector::class,
-        AddSeeTestAnnotationRector::class,
-        RemoveUnusedFunctionRector::class,
-    ]);
-    $parameters->set(Option::PATHS, [
-        'config',
-        'src',
-        'tests',
-    ]);
+    $parameters->set(
+        Option::SETS,
+        [
+            SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION,
+            SetList::ARRAY_STR_FUNCTIONS_TO_STATIC_CALL,
+            SetList::CELEBRITY,
+            //        'doctrine',
+            SetList::PHPSTAN,
+            SetList::PHPUNIT_CODE_QUALITY,
+            SetList::SOLID,
+            SetList::DOCTRINE_CODE_QUALITY,
+            SetList::DEAD_CODE,
+            SetList::CODE_QUALITY,
+            SetList::PHP_70,
+            SetList::PHP_71,
+            SetList::PHP_72,
+        ]
+    );
+    $parameters->set(
+        Option::EXCLUDE_RECTORS,
+        [
+            FinalizeClassesWithoutChildrenRector::class,
+            ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
+            RecastingRemovalRector::class,
+            RepeatedLiteralToClassConstantRector::class,
+            AddSeeTestAnnotationRector::class,
+            RemoveUnusedFunctionRector::class,
+        ]
+    );
+    $parameters->set(
+        Option::PATHS,
+        [
+            'config',
+            'src',
+            'tests',
+            'ecs.php',
+            'rector.php',
+        ]
+    );
 };
