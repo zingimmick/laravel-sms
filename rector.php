05d52f22d36f89b651ccd1ea8bdc8006fb85fb8b
zingimmick <zingimmick@outlook.com>
2022-04-19 22:21:55 +0800

    Add support for new ECSConfig and RectorConfig class

--- rector.php
+++ rector.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
@@ -10,28 +10,19 @@ use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Zing\CodingStandard\Set\RectorSetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(RectorSetList::CUSTOM);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_73);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PARALLEL, true);
-    $parameters->set(
-        Option::SKIP,
-        [
-            RenameVariableToMatchMethodCallReturnTypeRector::class,
-            RenameParamToMatchTypeRector::class,
-            AddSeeTestAnnotationRector::class,
-            FinalizeClassesWithoutChildrenRector::class,
-            PrivatizeLocalGetterToPropertyRector::class,
-        ]
-    );
-    $parameters->set(
-        Option::PATHS,
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([RectorSetList::CUSTOM, PHPUnitSetList::PHPUNIT_CODE_QUALITY, LevelSetList::UP_TO_PHP_73]);
+    $rectorConfig->parallel();
+    $rectorConfig->skip([
+        RenameVariableToMatchMethodCallReturnTypeRector::class,
+        RenameParamToMatchTypeRector::class,
+        AddSeeTestAnnotationRector::class,
+        FinalizeClassesWithoutChildrenRector::class,
+        PrivatizeLocalGetterToPropertyRector::class,
+    ]);
+    $rectorConfig->paths(
         [__DIR__ . '/config', __DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
     );
 };
