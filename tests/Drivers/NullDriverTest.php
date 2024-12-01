9fc27fa7a4c41411eb7af8bf499b39cfe5a8136b
Zing <zingimmick@outlook.com>
2024-12-01 23:18:13 +0800

    Update NullDriverTest.php

--- tests/Drivers/NullDriverTest.php
+++ tests/Drivers/NullDriverTest.php
@@ -19,6 +19,7 @@ final class NullDriverTest extends TestCase
     {
         $phoneNumber = new PhoneNumber(18_188_888_888);
         $message = SmsMessage::text('【test】This is a test message.');
+        Log::shouldReceive('channel')->passthru();
         Log::shouldReceive('debug')->withAnyArgs()->twice();
         Sms::connection('null')->send($phoneNumber, $message);
     }
