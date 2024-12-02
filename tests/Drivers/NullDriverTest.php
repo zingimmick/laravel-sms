368b5ee63b1fef5fc02433ba41678a6ddf946cde
Zing <zingimmick@outlook.com>
2024-12-02 23:52:14 +0800

    Update NullDriverTest.php

--- tests/Drivers/NullDriverTest.php
+++ tests/Drivers/NullDriverTest.php
@@ -1,30 +1,21 @@
 <?php
-
 declare(strict_types=1);
-
 namespace Zing\LaravelSms\Tests\Drivers;
-
 use Illuminate\Support\Facades\Log;
 use Overtrue\EasySms\PhoneNumber;
 use Zing\LaravelSms\Facades\Sms;
 use Zing\LaravelSms\SmsMessage;
-use Zing\LaravelSms\Tests\TestCase;use PHPUnit\Framework\Attributes\WithoutErrorHandler;
+use Zing\LaravelSms\Tests\TestCase;
 
 /**
  * @internal
  */
 final class NullDriverTest extends TestCase
 {
-    #[WithoutErrorHandler]
     public function testSend(): void
     {
         $phoneNumber = new PhoneNumber(18_188_888_888);
         $message = SmsMessage::text('【test】This is a test message.');
-        $logChannel = \Mockery::mock();
-        $logChannel->shouldReceive('warning')->withArgs(function(){
-var_dump(func_get_args());
-        }                             );
-        Log::shouldReceive('channel')->once()->with('deprecations')->andReturn($logChannel);
         Log::shouldReceive('debug')->withAnyArgs()->twice();
         Sms::connection('null')->send($phoneNumber, $message);
     }
