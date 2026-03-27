2e51dc534564cc8c1dc25f5d7d413d8a7ce6bde5
Zing <zingimmick@outlook.com>
2026-03-27 21:13:11 +0800

    Support for Laravel 13 (#334)

--- ecs.php
+++ ecs.php
@@ -8,6 +8,9 @@ use Zing\CodingStandard\Set\ECSSetList;
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([ECSSetList::PHP_80, ECSSetList::CUSTOM]);
     $ecsConfig->parallel();
+    $ecsConfig->skip([
+        \PhpCsFixer\Fixer\FunctionNotation\StaticLambdaFixer::class => [__DIR__ . '/src/SmsServiceProvider.php'],
+    ]);
     $ecsConfig->paths(
         [__DIR__ . '/config', __DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
     );
